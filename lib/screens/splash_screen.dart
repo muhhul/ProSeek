bcb9a28afaba861b3bad4f203d1e2ca4d86dd7b7
Muhammad Fatihul Irhab <fatihulirhab@gmail.com>
2024-01-12 11:32:37 +0700

    push

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class SplashScreen extends StatelessWidget{
   const SplashScreen({super.key});
@@ -9,23 +10,29 @@ class SplashScreen extends StatelessWidget{
     return Scaffold(
       body: Stack(children: [
         Image.asset('assets/images/Rectangle 910.png'),
-        SafeArea(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              const SizedBox(
-                height: 243,
-              ),
-              SvgPicture.asset('assets/svgs/logo.svg',height: 58),
-              const SizedBox(
-                height: 10,
-              ),
-              RichText(text: TextSpan(
-                text: "Profession Seeker\n",
-                children: [TextSpan(text: "Let’s find your dream job and improve your skill")]
-                ))
-            ],
-          ))
+        Center(
+          child: SafeArea(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const SizedBox(
+                  height: 243,
+                ),
+                SvgPicture.asset('assets/svgs/logo.svg',height: 58),
+                const SizedBox(
+                  height: 10,
+                ),
+                RichText(
+                  textAlign: TextAlign.center,
+                  text: TextSpan(
+                  text: "Profession Seeker\n", 
+                  style: GoogleFonts.inter(fontSize: 30, color: Color(0xFFFFFFFF), letterSpacing: -0.24, fontWeight: FontWeight.w700),
+                  children: [TextSpan(text: "Let’s find your dream job and improve your skill",
+                  style: GoogleFonts.lora(fontSize: 12, color: Color(0xFFFFFFFF), letterSpacing: -0.24, fontWeight: FontWeight.normal ))]
+                  ))
+              ],
+            )),
+        )
         ]),
     );
   }
